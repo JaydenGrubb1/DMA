3da348a5f62c4e828afdc8ff2463f5b064ba9fc6
Jayden Grubb <jaydengrubb1@gmail.com>
2023-10-08 16:36:42 +1000

    Converted DMA-Core to a static library

--- DMA-CLI/main.cpp
+++ DMA-CLI/main.cpp
@@ -1,11 +1,44 @@
 #include <cstdio>
 #include <cstdlib>
+#include <fstream>
+#include <vector>
 
-#include "core.h"
+#include "fft.h"
+#include "wav.h"
+
+using namespace DMA;
 
 int main(int argc, char** argv) {
-	printf("Invoking DMA-Core...\n");
-	DMA::Test();
+	Audio::WAV wav("../../sample.wav");
+
+	auto num_samples = wav.size() / wav.sample_size();
+	std::vector<complex> in(num_samples);
+	std::vector<complex> out(num_samples);
+
+	for (size_t i = 0; i < num_samples; i += wav.sample_size()) {
+		in[i] = complex((float)wav.data()[i], 0.0);
+	}
+
+	FFT::init();
+	FFT::stft(in, out);
+
+	std::ofstream stream;
+	stream.open("../../output.csv");
+
+	std::vector<float> freq(out.size() / 2);
+	int chunks = out.size() / FFT::WINDOW_SIZE;
+	int chunk_size = FFT::WINDOW_SIZE / 2;
+
+	for (int i = 0; i < chunks; i++) {
+		for (int j = 0; j < chunk_size; j++) {
+			float magnitude = std::abs(out[i * FFT::WINDOW_SIZE + j]);
+			stream << magnitude << ",";
+			freq[i * chunk_size + j] = magnitude;
+		}
+		stream << "\n";
+	}
+
+	stream.close();
 
 	return EXIT_SUCCESS;
 }
