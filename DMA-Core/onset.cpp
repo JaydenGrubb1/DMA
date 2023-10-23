ccc282121627a5fce11ce245ab943cbbadae7604
Jayden Grubb <jaydengrubb1@gmail.com>
2023-10-23 23:02:15 +1000

    Added Onset::detect() method signature

--- DMA-Core/onset.cpp
+++ DMA-Core/onset.cpp
@@ -1,1 +1,10 @@
+#define _USE_MATH_DEFINES
+#include <cmath>
+
 #include "onset.h"
+
+namespace DMA::Onset {
+	void detect(std::vector<float>& in, std::vector<float>& out) {
+		// TODO: Implement onset detection
+	}
+}
--- DMA-Core/onset.h
+++ DMA-Core/onset.h
@@ -1,2 +1,7 @@
 #pragma once
 
+#include <vector>
+
+namespace DMA::Onset {
+	void detect(std::vector<float>& in, std::vector<float>& out);
+}
